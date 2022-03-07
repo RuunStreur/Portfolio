4d71403fde043382e65627814ede100da46f55d7
Ruun <ruunstreur@gmail.com>
2022-03-07 11:58:31 +0100

    testfile update

--- Testfiles/Testfile_2.R
+++ Testfiles/Testfile_2.R
@@ -1,50 +1,78 @@
-bowie <- get_playlist_audio_features("", "109zncLRngc5ofl1DsCHcE")
-bowie_eno <- get_playlist_audio_features("", "6Rb67d7vVnq1FFAASwy5p5")
-TH <- get_playlist_audio_features("", "1K27mtJcCFQw4IxJLZlmNQ")
-TH_eno <- get_playlist_audio_features("", "4XrUACEdnQh8WfygAgSKxd")
+cap <- get_tidy_audio_analysis("0yhwdmbgkKdE1plV8xWdrd") 
+heroes <- get_tidy_audio_analysis("5Ci3b8pfpLA9Zk17qGXBCF")
+bop <- get_tidy_audio_analysis("4eBCTzBsSjYgrLH5clQf2x")
+sans <- get_tidy_audio_analysis("58uyNQ2aC329uLMZpU9Ptz")
+fela <- get_tidy_audio_analysis("0Q4O6v2akT3RfkNxCt522f")
+place <- get_tidy_audio_analysis("6aBUnkXuCEQQHAlTokv9or")
 
-bowieSum <- bowie%>%
-  summarize(meanVal = mean(danceability)) %>% add_column(is_eno = 0, artist = 'bowie')
-bowie_enoSum <- bowie_eno%>%
-  summarize(meanVal = mean(danceability)) %>% add_column(is_eno = 1, artist = 'bowie')
-THSum <- TH%>%
-  summarize(meanVal = mean(danceability)) %>% add_column(is_eno = 0, artist = 'TH')
-TH_enoSum <- TH_eno%>%
-  summarize(meanVal = mean(danceability)) %>% add_column(is_eno = 1, artist = 'TH')
-all_sum1 <- bind_rows(bowieSum, bowie_enoSum, THSum, TH_enoSum)
+psycho <- get_tidy_audio_analysis("5cE2nMYDgYj9gqG4KLafKy")
+nf <- get_tidy_audio_analysis("2pmytOddxLVaASHQqTH9LO")
+aypt <- get_tidy_audio_analysis("7Js4OF5MUb2bqJe09g4uQE")
+hill <- get_tidy_audio_analysis("1raM9iX3HPhZvINoYCSWc9")
+ata <- get_tidy_audio_analysis("37CRPk0L5ZpfPeePEPwE0t")
 
-bowie_val <- subset(all_sum1, artist=="bowie" & is_eno=="0")$meanVal
-bowie_eno_val <- subset(all_sum1, artist=="bowie" & is_eno=="1")$meanVal
-TH_val <- subset(all_sum1, artist=="TH" & is_eno=="0")$meanVal
-TH_eno_val <- subset(all_sum1, artist=="TH" & is_eno=="1")$meanVal
-
-bowieval <- data.frame(artist = 'David Bowie', without_eno = bowie_val, with_eno = bowie_eno_val)
-THval <- data.frame(artist = 'Talking Heads', without_eno = TH_val, with_eno = TH_eno_val)
-all_sum <- bind_rows(bowieval, THval)
-
-colnames(all_sum) <- c("artist", "without_eno", "with_eno")
-left_label <- paste(all_sum$artist, all_sum$`without_eno`,sep=", ")
-right_label <- paste(all_sum$artist, all_sum$`with_eno`,sep=", ")
-all_sum$class <- ifelse((all_sum$`with_eno` - all_sum$`without_eno`) < 0, "red", "green")
-
-p <- ggplot(all_sum) + geom_segment(aes(x=1, xend=2, y=`without_eno`, yend=`with_eno`, col=class), size=.75, show.legend=F) + 
-  geom_vline(xintercept=1, linetype="dashed", size=.1) + 
-  geom_vline(xintercept=2, linetype="dashed", size=.1) +
-  scale_color_manual(labels = c("Up", "Down"), 
-                     values = c("green"="#00ba38", "red"="#f8766d")) +
-  labs(x="", y="Mean danceability") +  # Axis labels
-  xlim(.5, 2.5) + ylim(0.4,(1.1*(max(all_sum$`without_eno`, all_sum$`with_eno`))))
-
-p <- p + geom_text(label=left_label, y=all_sum$`without_eno`, x=rep(1, NROW(all_sum)), hjust=1.1, size=3.5)
-p <- p + geom_text(label=right_label, y=all_sum$`with_eno`, x=rep(2, NROW(all_sum)), hjust=-0.1, size=3.5)
-p <- p + geom_text(label="Without Eno", x=1, y=1.1*(max(all_sum$`without_eno`, all_sum$`with_eno`)), hjust=1.2, size=5)  # title
-p <- p + geom_text(label="With Eno", x=2, y=1.1*(max(all_sum$`without_eno`, all_sum$`with_eno`)), hjust=-0.1, size=5)  # title
-
-p + theme(axis.ticks = element_blank(),
-          axis.text.x = element_blank(),
-          plot.margin = unit(c(1,2,1,2), "cm"))
-  labs(
-    title = 'The mean danceability of Eno vs. non-Eno produced albums'
+song <-
+  heroes %>% # Change URI.
+  compmus_align(bars, segments) %>%                     # Change `bars`
+  select(bars) %>%                                      #   in all three
+  unnest(bars) %>%                                      #   of these lines.
+  mutate(
+    pitches =
+      map(segments,
+          compmus_summarise, pitches,
+          method = "rms", norm = "manhattan"              # Change summary & norm.
+      )
+  ) %>%
+  mutate(
+    timbre =
+      map(segments,
+          compmus_summarise, timbre,
+          method = "rms", norm = "manhattan"              # Change summary & norm.
+      )
   )
+
+#cepto
+
+  
+cepto1 <- song %>%
+  compmus_gather_timbre() %>%
+  ggplot(
+    aes(
+      x = start + duration / 2,
+      width = duration,
+      y = basis,
+      fill = value
+    )
+  ) +
+  geom_tile() +
+  labs(x = "Time (s)", y = NULL, fill = "Magnitude") +
+  scale_fill_viridis_c() +                              
+  theme_classic()
+
+cepto1
+#ssm
+
+ssm <- song %>%
+  compmus_self_similarity(timbre, "aitchison") %>% 
+  ggplot(
+    aes(
+      x = xstart + xduration / 2,
+      width = xduration,
+      y = ystart + yduration / 2,
+      height = yduration,
+      fill = d
+    )
+  ) +
+  geom_tile() +
+  coord_fixed() +
+  scale_fill_viridis_c(guide = "none") +
+  theme_classic() +
+  labs( title = "Self Similarity Matrix of Bowie's 'Heroes'", 
+       x = "",
+       y = "")
+
+ssm
+
+
 
 
--- Testfiles/testing_file.R
+++ Testfiles/testing_file.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(plyr)
 library(spotifyr)
 library(ggplot2)
 library(compmus)
@@ -10,6 +9,10 @@ bowie_eno <- get_playlist_audio_features("", "6Rb67d7vVnq1FFAASwy5p5")
 TH <- get_playlist_audio_features("", "1K27mtJcCFQw4IxJLZlmNQ")
 TH_eno <- get_playlist_audio_features("", "4XrUACEdnQh8WfygAgSKxd")
 #bowie_disc <- get_playlist_audio_features("", "0mkz40ZahTeC6y2klc527h")
+
+bowie$track.artists[1][[1]][3]
+b[[1]][3]
+
 
 bowie_extracol <- bowie %>% add_column(is_eno = 0)
 bowie_eno_extracol <- bowie_eno %>% add_column(is_eno = 1)
@@ -126,42 +129,55 @@ compmus_long_distance(
   theme_minimal() +
   scale_fill_viridis_c(guide = NULL)
 
+######################
 
+bowie <- get_playlist_audio_features("", "109zncLRngc5ofl1DsCHcE")
+bowie_eno <- get_playlist_audio_features("", "6Rb67d7vVnq1FFAASwy5p5")
+TH <- get_playlist_audio_features("", "1K27mtJcCFQw4IxJLZlmNQ")
+TH_eno <- get_playlist_audio_features("", "4XrUACEdnQh8WfygAgSKxd")
 
+bowieSum <- bowie%>%
+  summarize(meanVal = mean(danceability)) %>% add_column(is_eno = 0, artist = 'bowie')
+bowie_enoSum <- bowie_eno%>%
+  summarize(meanVal = mean(danceability)) %>% add_column(is_eno = 1, artist = 'bowie')
+THSum <- TH%>%
+  summarize(meanVal = mean(danceability)) %>% add_column(is_eno = 0, artist = 'TH')
+TH_enoSum <- TH_eno%>%
+  summarize(meanVal = mean(danceability)) %>% add_column(is_eno = 1, artist = 'TH')
+all_sum1 <- bind_rows(bowieSum, bowie_enoSum, THSum, TH_enoSum)
 
+bowie_val <- subset(all_sum1, artist=="bowie" & is_eno=="0")$meanVal
+bowie_eno_val <- subset(all_sum1, artist=="bowie" & is_eno=="1")$meanVal
+TH_val <- subset(all_sum1, artist=="TH" & is_eno=="0")$meanVal
+TH_eno_val <- subset(all_sum1, artist=="TH" & is_eno=="1")$meanVal
 
+bowieval <- data.frame(artist = 'David Bowie', without_eno = bowie_val, with_eno = bowie_eno_val)
+THval <- data.frame(artist = 'Talking Heads', without_eno = TH_val, with_eno = TH_eno_val)
+all_sum <- bind_rows(bowieval, THval)
 
+colnames(all_sum) <- c("artist", "without_eno", "with_eno")
+left_label <- paste(all_sum$artist, all_sum$`without_eno`,sep=", ")
+right_label <- paste(all_sum$artist, all_sum$`with_eno`,sep=", ")
+all_sum$class <- ifelse((all_sum$`with_eno` - all_sum$`without_eno`) < 0, "red", "green")
 
-library(ggplot2)
-library(scales)
-theme_set(theme_classic())
-
-# prep data
-df <- read.csv("https://raw.githubusercontent.com/selva86/datasets/master/gdppercap.csv")
-colnames(df) <- c("continent", "1952", "1957")
-left_label <- paste(df$continent, round(df$`1952`),sep=", ")
-right_label <- paste(df$continent, round(df$`1957`),sep=", ")
-df$class <- ifelse((df$`1957` - df$`1952`) < 0, "red", "green")
-
-# Plot
-p <- ggplot(df) + geom_segment(aes(x=1, xend=2, y=`1952`, yend=`1957`, col=class), size=.75, show.legend=F) + 
+p <- ggplot(all_sum) + geom_segment(aes(x=1, xend=2, y=`without_eno`, yend=`with_eno`, col=class), size=.75, show.legend=F) + 
   geom_vline(xintercept=1, linetype="dashed", size=.1) + 
   geom_vline(xintercept=2, linetype="dashed", size=.1) +
   scale_color_manual(labels = c("Up", "Down"), 
-                     values = c("green"="#00ba38", "red"="#f8766d")) +  # color of lines
-  labs(x="", y="Mean GdpPerCap") +  # Axis labels
-  xlim(.5, 2.5) + ylim(0,(1.1*(max(df$`1952`, df$`1957`))))  # X and Y axis limits
+                     values = c("green"="#00ba38", "red"="#f8766d")) +
+  labs(x="", y="Mean danceability") +  # Axis labels
+  xlim(.5, 2.5) + ylim(0.4,(1.1*(max(all_sum$`without_eno`, all_sum$`with_eno`))))
 
-# Add texts
-p <- p + geom_text(label=left_label, y=df$`1952`, x=rep(1, NROW(df)), hjust=1.1, size=3.5)
-p <- p + geom_text(label=right_label, y=df$`1957`, x=rep(2, NROW(df)), hjust=-0.1, size=3.5)
-p <- p + geom_text(label="Time 1", x=1, y=1.1*(max(df$`1952`, df$`1957`)), hjust=1.2, size=5)  # title
-p <- p + geom_text(label="Time 2", x=2, y=1.1*(max(df$`1952`, df$`1957`)), hjust=-0.1, size=5)  # title
+p <- p + geom_text(label=left_label, y=all_sum$`without_eno`, x=rep(1, NROW(all_sum)), hjust=1.1, size=3.5)
+p <- p + geom_text(label=right_label, y=all_sum$`with_eno`, x=rep(2, NROW(all_sum)), hjust=-0.1, size=3.5)
+p <- p + geom_text(label="Without Eno", x=1, y=1.1*(max(all_sum$`without_eno`, all_sum$`with_eno`)), hjust=1.2, size=5)  # title
+p <- p + geom_text(label="With Eno", x=2, y=1.1*(max(all_sum$`without_eno`, all_sum$`with_eno`)), hjust=-0.1, size=5)  # title
 
-# Minify theme
-p + theme(panel.background = element_blank(), 
-          panel.grid = element_blank(),
-          axis.ticks = element_blank(),
+p + theme(axis.ticks = element_blank(),
           axis.text.x = element_blank(),
-          panel.border = element_blank(),
           plot.margin = unit(c(1,2,1,2), "cm"))
+labs(
+  title = 'The mean danceability of Eno vs. non-Eno produced albums'
+)
+
+
